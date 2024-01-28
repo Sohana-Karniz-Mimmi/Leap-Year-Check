960cdbfa6e6d79b5e0836546f69b50827025a992
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-01-28 23:41:56 +0600

    added js file

--- leapYear.js
+++ leapYear.js
@@ -1,4 +1,4 @@
-let year = prompt("Enter a single character:");
+let year = prompt("Enter a Year:");
 
 if(((year % 4 === 0) && (year % 100 !== 0)) || ((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0))){
     document.write(`${year} is a leap year`)
